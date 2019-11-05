e994280af4de14c95a46f4cabb69c11c6b88f459
Patrik Mäsiar <patrik@kontentino.com>
2019-11-05 13:50:54 +0100

    older browsers syntax support

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use-strict';
 
-export default (...inputArguments) => {
-  return Array.from(inputArguments).filter(item => !!item).join(" ");
+export default function(){
+  return Array.from(arguments).filter(function(item) {
+    return !!item;
+  }).join(" ");
 }
