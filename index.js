7e51c5809b2f20cfe9becd41372c0b7f968c3158
Ivan Hanák <hanakivan@gmail.com>
2020-01-18 12:38:48 +0100

    Added support for arrays

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 module.exports = function(){
   return Array.from(arguments).filter(function(item) {
+    if(Array.isArray(item)) {
+      return item.length > 0;
+    }
     return !!item;
+  }).map(function (item) {
+    if(Array.isArray(item)) {
+      return item.join(" ");
+    }
+
+    return item;
   }).join(" ");
-}
+};
